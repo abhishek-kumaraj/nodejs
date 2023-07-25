fe701c32010633696a49036f317a0d3360668cb5
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-07-25 18:24:13 +0530

    added a mindBlowing.js file

--- 2-module.js
+++ 2-module.js
@@ -6,6 +6,8 @@
 const Hello = require('./4-fun') // importing the sayHello function of fun.js module
 const names = require('./5-names') // importing the exports object of name.js module
 const obj = require('./6-alterWayToExport') // importing the exports object of alterWayToExport.js module
+require('./7-mindGranade')
+
 Hello(`${names.firstName} ${names.lastName}`)
 Hello('Samraat')
 console.log(obj);
---
+++ 7-mindGranade.js
@@ -0,0 +1,7 @@
+const num1 = 3;
+const num2 = 4;
+
+const product = function(){
+    console.log(`Product of two numbers is ${num1 * num2}`);
+}
+product();
