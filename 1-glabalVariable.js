556de75ed3385c6909cdf36a704c4ed41f2ff8c7
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-07-25 15:09:25 +0530

    added global variable file

---
+++ 1-glabalVariable.js
@@ -0,0 +1,9 @@
+// global variable are the ones which can we accessed
+// from any where in the program without importing explicitly
+
+// __filename - print the name of current file with its complete address
+// __dirname - print the path of current directory
+// require - a method used to import modules
+
+console.log(__filename);
+console.log(__dirname);
