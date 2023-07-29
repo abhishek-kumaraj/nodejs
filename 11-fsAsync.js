f4febfe7ed1f2cd8f922dce72a5f283d66f6d28e
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-07-29 17:55:27 +0530

    illustrated the working of async behaviour of js

--- 11-fsAsync.js
+++ 11-fsAsync.js
@@ -2,6 +2,9 @@
 
 const {readFile, writeFile} = require('fs') // importing two functions of fs module
 
+console.log('start of first task')
+
+// this readfile code will execute parallely with rest of the code 
 readFile('./folder/subfolder/text.txt','utf8',(err, result)=>{
     if(err) // if error occured while reading the file
     {
@@ -14,7 +17,8 @@ readFile('./folder/subfolder/text.txt','utf8',(err, result)=>{
             console.log(err)
             return
         }
-        return result
+        console.log('task is completed')
     })
 
 } ) // after reading the specified file path , executing the callback function
+console.log('start of next task')
