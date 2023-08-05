5760e828385c3d78cc8a3dcec32bbc60c030d1e0
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-08-06 00:51:17 +0530

    added query String parameter code

--- 21-apiBasic.js
+++ 21-apiBasic.js
@@ -6,6 +6,7 @@ const {products} = require('./json.js') // importing products variable from json
 app.get('/',(req,res)=>{ 
     res.send('<h2>Home page</h2><a href ="/api/products">Products</a>')
 })
+// get method for /api/products route
 app.get('/api/products',(req,res)=>{
     const newProducts = products.map((product)=>{
         const {id,name,price,desc} = product
@@ -13,13 +14,32 @@ app.get('/api/products',(req,res)=>{
     })
     res.json(newProducts)
 })
-
+// get method for route params to get specific result
 app.get('/api/products/:productID',(req,res)=>{
     console.log(req.params) 
     const {productID} = req.params
     const singleProduct = products.find((product)=> product.id === Number(productID))
     
     res.json(singleProduct)
+})
+// setting up query string to provide personalised result
+app.get('/api/v1/query', (req,res)=>{
+    console.log(req.query)
+    let sortedArray = [...products]
+    const {start,limit}= req.query
+    if(start){
+        sortedArray = sortedArray.filter((product)=>{
+            return product.name.startsWith(start)
+        })
+    }
+    if(limit){
+        sortedArray = sortedArray.slice(0,Number(limit))
+    }
+    if(sortedArray.length < 1){
+        return res.status(200).json({sucess:true,data:[]})
+    }
+    res.json(sortedArray)
+
 })
 
 app.listen(5000,()=>{
