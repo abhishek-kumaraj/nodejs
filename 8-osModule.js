9f61a1939e198e7a4ef2ec836ab44ff5c3e1655a
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-08-08 01:30:04 +0530

    added comment in osModule.js file

--- 8-osModule.js
+++ 8-osModule.js
@@ -1,19 +1,17 @@
-// build in modules
+// os module - build-in module used to interact with the operating system and also retrive information about it
+const os = require('os'); // importing os module
 
-// os module
-const os = require('os');
-
-const user = os.userInfo();
+const user = os.userInfo(); // it returns an object contaning info about the current user like username, user id
 console.log(user);
 
-const timeSec = os.uptime()
+const timeSec = os.uptime()// returns the system uptime in seconds
 const timeHour = ((timeSec / 60) /60)
 console.log(timeHour)
 
 const currentOs = {
     name: os.type(),
     release: os.release(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem()
+    totalMem: os.totalmem(), // returns the system total memory
+    freeMem: os.freemem() // return the free system memory
 }
 console.log(currentOs)
