d0782a2b8207840a441f6c043efc1e7435c5d06b
abhishek-kumaraj <abhishekkumaraj1506@gmail.com>
2023-08-08 01:16:49 +0530

    updated comment and imported a variable in module.js file

--- 2-module.js
+++ 2-module.js
@@ -1,14 +1,16 @@
 // every file is module in nodejs
-// modules are used to encapsulate code , to break problem in smaller parts
+// modules are used to encapsulate code , to break down problem in smaller parts
 
 // by default every variable or funtions are of private scope means visible only inside that module only
 
 const Hello = require('./4-fun') // importing the sayHello function of fun.js module
 const names = require('./5-names') // importing the exports object of name.js module
+const {firstName} = require('./5-names') // importing firstName variable of name.js module
 const obj = require('./6-alterWayToExport') // importing the exports object of alterWayToExport.js module
 require('./7-mindGranade') // going to execute the whole module 
 
 Hello(`${names.firstName} ${names.lastName}`)
+Hello(firstName)
 Hello('Samraat')
 console.log(obj);
 
